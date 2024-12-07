85d9deee1128dea5b6137f713a9c35537ddf6013
AzarAI <azarai@mail.ustc.edu.cn>
2024-12-07 14:26:58 +0800

    Initialize the configurations

---
+++ lua/plugins/treesitter.lua
@@ -0,0 +1,16 @@
+-- Customize Treesitter
+
+---@type LazySpec
+return {
+  "nvim-treesitter/nvim-treesitter",
+  opts = {
+    ensure_installed = {
+      "lua",
+      "vim",
+      "c",
+      "markdown",
+      "toml",
+      "yaml",
+    },
+  },
+}
