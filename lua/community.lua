dfa88333c07bd44ca0a2f63c33d58752f7880d4a
AzarAI <azarai@mail.ustc.edu.cn>
2025-01-09 20:58:17 +0800

    Update: remove useless comment

--- lua/community.lua
+++ lua/community.lua
@@ -5,7 +5,6 @@
 ---@type LazySpec
 return {
   "AstroNvim/astrocommunity",
-  -- { import = "astrocommunity.pack.lua" },
-  -- import/override with your plugins folder
+  -- Import/override with your plugins folder
   { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
 }
