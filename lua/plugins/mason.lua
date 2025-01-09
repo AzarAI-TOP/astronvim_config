935999f385078f764f840b2454d15209b4bb1526
AzarAI <azarai@mail.ustc.edu.cn>
2025-01-09 21:00:23 +0800

    Update: Upper case all comments

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -2,35 +2,35 @@
 
 ---@type LazySpec
 return {
-  -- use mason-lspconfig to configure LSP installations
+  -- Use mason-lspconfig to configure LSP installations
   {
     "williamboman/mason-lspconfig.nvim",
-    -- overrides `require("mason-lspconfig").setup(...)`
+    -- Overrides `require("mason-lspconfig").setup(...)`
     opts = {
       ensure_installed = {
         "lua_ls",
-        -- add more arguments for adding more language servers
+        -- Add more arguments for adding more language servers
       },
     },
   },
-  -- use mason-null-ls to configure Formatters/Linter installation for null-ls sources
+  -- Use mason-null-ls to configure Formatters/Linter installation for null-ls sources
   {
     "jay-babu/mason-null-ls.nvim",
-    -- overrides `require("mason-null-ls").setup(...)`
+    -- Overrides `require("mason-null-ls").setup(...)`
     opts = {
       ensure_installed = {
         "stylua",
-        -- add more arguments for adding more null-ls sources
+        -- Add more arguments for adding more null-ls sources
       },
     },
   },
   {
     "jay-babu/mason-nvim-dap.nvim",
-    -- overrides `require("mason-nvim-dap").setup(...)`
+    -- Overrides `require("mason-nvim-dap").setup(...)`
     opts = {
       ensure_installed = {
         "python",
-        -- add more arguments for adding more debuggers
+        -- Add more arguments for adding more debuggers
       },
     },
   },
