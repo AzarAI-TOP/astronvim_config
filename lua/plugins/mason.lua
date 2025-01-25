5ec722525414995f59e545d3bd8c68f437771236
AzarAI <azarai@mail.ustc.edu.cn>
2025-01-25 15:00:35 +0800

    Update: add c/cpp language server

--- lua/plugins/mason.lua
+++ lua/plugins/mason.lua
@@ -9,7 +9,7 @@ return {
     opts = {
       ensure_installed = {
         "lua_ls",
-        -- Add more arguments for adding more language servers
+        "clangd",
       },
     },
   },
@@ -20,7 +20,7 @@ return {
     opts = {
       ensure_installed = {
         "stylua",
-        -- Add more arguments for adding more null-ls sources
+        "clang-format",
       },
     },
   },
@@ -30,7 +30,6 @@ return {
     opts = {
       ensure_installed = {
         "python",
-        -- Add more arguments for adding more debuggers
       },
     },
   },
